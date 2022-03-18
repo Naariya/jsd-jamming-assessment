cc7bfb91f98fabab589a7844243c8e9a44f73302
Nannapat Ariyakornwijit <nannapat9858@gmail.com>
2022-03-17 17:26:05 -0700

    update URL

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,7 +1,7 @@
 // Get Client ID from https://developer.spotify.com/dashboard/ and put it here
 const clientId = '895147a5897b4e2f98452fe6e123f282';
 
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = 'https://naariya-jammming.netlify.app';
 const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientId}&redirect_uri=${redirectUri}`;
 let accessToken = undefined;
 let expiresIn = undefined;
