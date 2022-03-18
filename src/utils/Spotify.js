cb1f7e2ceb92744008fe872485bb56f17aca84ce
Nannapat Ariyakornwijit <nannapat9858@gmail.com>
2022-03-17 17:32:24 -0700

    update spotify

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -70,7 +70,7 @@ const Spotify = {
             Authorization: `Bearer ${accessToken}`,
           },
           body: JSON.stringify({
-            uris: trackIds.map((id) => 'spotify:track:'.concat(id)),
+            uris: trackIds,
           }),
         });
       }
